7e3233064e717cafe6fbb980fb3cbe1285512a7a
JakubBogacz <jakub.bogacz@vavatech.pl>
2017-11-20 15:52:43 +0100

    ngFor class.active

--- Spotify/src/app/list-playlists/list-playlists.component.ts
+++ Spotify/src/app/list-playlists/list-playlists.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+interface Playlist {
+  id: number,
+  name: string,
+  description: string,
+  favorite: boolean,
+  color: string
+
+}
 
 @Component({
   selector: 'component-list-playlists',
@@ -8,7 +17,43 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ListPlaylistsComponent implements OnInit {
 
-  constructor() { }
+  title: string = 'Playlist list';
+
+  playlists: Playlist[] = [
+    {
+      id: 1,
+      name: "Hity lat 90",
+      description: "Opis listy hitow lat 80",
+      favorite: true,
+      color: "#ff0000"
+    },
+    {
+      id: 2,
+      name: "Hity lat 80",
+      description: "Opis listy hitow lat 70",
+      favorite: false,
+      color: "#00ff00"
+    },
+    {
+      id: 3,
+      name: "Hity lat 70",
+      description: "Opis listy hitow lat 70",
+      favorite: false,
+      color: "#0000ff"
+    },
+    {
+      id: 4,
+      name: "Hity lat 60",
+      description: "Opis listy hitow lat 60",
+      favorite: false,
+      color: "#ff00ff"
+    },
+  ]
+
+  selectedPlaylist: Playlist;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
