f615d1c99c2eebdc88055ab93ba90709ed00a11f
Blazej <blazej.porwol@wp.eu>
2017-11-20 12:07:53 +0100

    prop binding events

---
+++ spotfyy/src/app/detail-playlist/detail-playlist.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'lekarz-detail-playlist',
+  templateUrl: './detail-playlist.component.html',
+  styles: [],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class DetailPlaylistComponent implements OnInit {
+
+  title : string = 'Playlist detail';
+  playlist: {
+    name: string,
+    description: string,
+    favorite: boolean,
+    color: string
+  } = {
+    name: 'Pierwsza',
+    description: 'Opis pierwszej',
+    favorite: false,
+    color: '#ff0000'
+  }
+  //playlistName : string = 'Pierwsza';
+  //playlistDescription : string = 'Opis pierwszej';
+  //playlistColor : string = '#ff0000';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
