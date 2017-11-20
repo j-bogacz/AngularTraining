43da5ebed7c597a9e3f34aa472bbcbde0f456c77
PAV41 <pawelkozbial41@gmail.com>
2017-11-20 15:56:55 +0100

    selecting elements from the list

--- spotifypk/src/app/list-play-lists/list-play-lists.component.ts
+++ spotifypk/src/app/list-play-lists/list-play-lists.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+interface playList {
+  id: number;
+  name: string;
+  description?: string;
+  favourite: boolean;
+  color: string;
+}
+
 @Component({
   selector: 'pawel-list-play-lists',
   templateUrl: './list-play-lists.component.html',
@@ -7,6 +15,15 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated
 })
 export class ListPlayListsComponent implements OnInit {
+
+  selectedItem: playList,
+
+  listPlayLists : playList[] = [
+    {id: 1, name: "Hity lat 80tych", description: "Opis", favourite: false, color: "#ff0000"},
+    {id: 2, name: "Hity lat 70tych", description: "Opis", favourite: false, color: "#ff0000"},
+    {id: 3, name: "Hity lat 60tych", description: "Opis", favourite: false, color: "#ff0000"},
+    {id: 4, name: "Hity lat 50tych", description: "Opis", favourite: false, color: "#ff0000"},
+  ];
 
   constructor() { }
 
