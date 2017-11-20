f8e5c438fec840d73ec44ce26d26e45aba3c5f8e
Thorgiliusz <immo.nemesis@gmail.com>
2017-11-20 15:53:10 +0100

    ngFor class.active

--- thorplay/src/app/list-playlists/list-playlists.component.ts
+++ thorplay/src/app/list-playlists/list-playlists.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+interface Playlist {
+  id: number;
+  name: string;
+  description: string;
+  favorite: boolean;
+  color: string;
+}
 
 @Component({
   selector: 'kuku-list-playlists',
@@ -7,6 +15,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated
 })
 export class ListPlaylistsComponent implements OnInit {
+  listPlaylists: Playlist[] = [
+    {id: 1, name: 'Hity lat 80tych', description: 'fajna lista z lat 80tych', favorite: false, color: '#00ff00'},
+    {id: 2, name: 'Hity lat 70tych', description: 'fajna lista z lat 70tych', favorite: false, color: '#ff0000'},
+    {id: 3, name: 'Hity lat 60tych', description: 'fajna lista z lat 60tych', favorite: true, color: '#0000ff'}
+  ];
+
+  currentPlaylist: Playlist;
 
   constructor() { }
 
