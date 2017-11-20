af5d7d0389e263dd885302959f46525b311cac38
darekb <kontakt@3wm.pl>
2017-11-20 15:52:50 +0100

    ngFor class.active

--- spotfyy/src/app/list-playlists/list-playlists.component.ts
+++ spotfyy/src/app/list-playlists/list-playlists.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+interface Playlist {
+  id: number;
+  name: string;
+  description: string;
+  favorite: boolean;
+  color: string;
+}
+
 @Component({
   selector: 'lekarz-list-playlists',
   templateUrl: './list-playlists.component.html',
@@ -7,7 +15,12 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated
 })
 export class ListPlaylistsComponent implements OnInit {
-
+  selected: Playlist;
+  listPlaylists: Playlist[] = [
+    {id: 1, name: 'Hity lat 80', description: 'Opis1', favorite: true, color: '#ff0000'},
+    {id: 2, name: 'Hity lat 70', description: 'Opis2', favorite: false, color: '#00ff00'},
+    {id: 3, name: 'Hity lat 60', description: 'Opis3', favorite: false, color: '#0000ff'},
+  ];
   constructor() { }
 
   ngOnInit() {
