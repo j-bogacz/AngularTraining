22efe7f4099ef8fd6f794b3749a21dc4451b70ee
boguslaw <b.pilek@gmail.com>
2017-11-20 14:54:02 +0100

    ngFor class.active

--- app1/src/app/list-play-list/list-play-list.component.ts
+++ app1/src/app/list-play-list/list-play-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+interface PlayList {
+  id: number,
+  name: string,
+  description: string,
+  favorite: boolean,
+  color: string
+}
 
 @Component({
   selector: 'abc-list-play-list',
@@ -8,7 +16,17 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ListPlayListComponent implements OnInit {
 
-  constructor() { }
+  selected: PlayList;
+
+  listPlayLists: PlayList[] = [
+    {id: 1, name: '100 Turbo Hits', description: 'Opis 1', favorite: false, color: '#435577'},
+    {id: 2, name: '200 Turbo Hits', description: 'Opis 2', favorite: false, color: '#43ff77'},
+    {id: 3, name: '300 Turbo Hits', description: 'Opis 3', favorite: false, color: '#ff5577'},
+    {id: 4, name: '400 Turbo Hits', description: 'Opis 4', favorite: false, color: '#4355ff'}
+    ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
