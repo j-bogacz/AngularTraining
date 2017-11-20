48dd671d01de38ba7aa815f82e82eee1d5b9b1b7
bartek_sikorski <bartek.sikorski@gmail.com>
2017-11-20 14:58:44 +0100

    html template

--- spotifybart/src/app/detail-playlist/detail-playlist.component.ts
+++ spotifybart/src/app/detail-playlist/detail-playlist.component.ts
@@ -3,7 +3,12 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'bart-detail-playlist',
   templateUrl: './detail-playlist.component.html',
-  styles: [],
+  styles: [` 
+    .card-title {
+      border-right: 2px solid transparent;
+    }
+  `
+  ],
   encapsulation: ViewEncapsulation.Emulated
 })
 export class DetailPlaylistComponent implements OnInit {
@@ -17,13 +22,13 @@ export class DetailPlaylistComponent implements OnInit {
     color: string
   } = {
     name: "Pierwsza",
-    description: "Opis pierszrj",
+    description: "Opis pierwszej",
     favourite: true,
     color: "#aa0000"
   };
-
+  editMode: boolean = false;
   playlistName: string = "Pierwsza";
-  playlistdescription: string = "Opis pierwszej";
+  playlistdescription: string = "Opis listy pierwszej";
   playlistColour: string = "#aa0000";
 
 
