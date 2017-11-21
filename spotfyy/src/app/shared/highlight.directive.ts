94e2b3b0f5aa61fd5199b09c426e32a83a30e4e8
darekb <kontakt@3wm.pl>
2017-11-21 13:19:10 +0100

    directive hostListener color geter

--- spotfyy/src/app/shared/highlight.directive.ts
+++ spotfyy/src/app/shared/highlight.directive.ts
@@ -1,27 +1,34 @@
 import {
   Directive,
   ElementRef,
+  HostBinding,
   HostListener,
   Input,
-  OnInit} from '@angular/core';
+  OnInit
+} from '@angular/core';
 
 @Directive({
   selector: '[lekarzHighlight]'
 })
 export class HighlightDirective implements OnInit {
+  hover: boolean = false;
   @Input('lekarzHighlight') highlightColor:string = 'black';
   @Input() defaultColor:string = 'green';
   constructor(private el: ElementRef) {
   }
   ngOnInit(){
-    this.el.nativeElement.style.borderLeftColor = this.defaultColor;
+    //this.el.nativeElement.style.borderLeftColor = this.defaultColor;
+  }
+  @HostBinding('style.borderLeftColor')
+  get getColor(){
+    return this.hover ? this.highlightColor : this.defaultColor;
   }
   @HostListener('mouseenter')
-  enter(){
-    this.el.nativeElement.style.borderLeftColor = this.highlightColor;
+  enter() {
+    this.hover = true;
   }
   @HostListener('mouseleave')
-  exit(){
-    this.el.nativeElement.style.borderLeftColor = this.defaultColor;
+  exit() {
+    this.hover = false;
   }
 }
