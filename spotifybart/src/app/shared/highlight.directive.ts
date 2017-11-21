f89144ea9dd9c1872fa076f8db37ba67f0bb45f5
bartek_sikorski <bartek.sikorski@gmail.com>
2017-11-21 13:21:06 +0100

    directive hostlistener color geter

--- spotifybart/src/app/shared/highlight.directive.ts
+++ spotifybart/src/app/shared/highlight.directive.ts
@@ -1,14 +1,14 @@
-import { Directive, ElementRef, Input, OnInit, HostListener} from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, HostListener, HostBinding, Renderer2} from '@angular/core';
 import { Playlist } from '../playlist';
 
 @Directive({
   selector: '[bartHighlight]'
 })
 export class HighlightDirective {
-
+  hover:boolean = false;
   @Input("bartHighlight") highligtColor:string = 'black';
   @Input() defaultColor:string = 'green';
-  constructor(private gl: ElementRef) {
+  constructor(private gl: ElementRef, private renderer: Renderer2) {
     console.log(gl);
     this.gl.nativeElement.style.borderLeftColor = this.defaultColor;
   }
@@ -17,14 +17,24 @@ export class HighlightDirective {
     console.log(this.highligtColor);
     this.gl.nativeElement.style.borderLeftColor = this.defaultColor;
   }
+
+  @HostBinding('style.borderLeftColor')
+  get getColor() {
+    return this.hover ? this.highligtColor : this.defaultColor;
+  }
+
   @HostListener('mouseenter')
   enter() {
-    this.gl.nativeElement.style.borderLeftColor = this.highligtColor;
+    //this.renderer.setStyle(this.gl.nativeElement,
+   //  'border-left-color',
+   //this.highligtColor);
+   this.hover = true;
+    //this.gl.nativeElement.style.borderLeftColor = this.highligtColor;
 
   }
 
   @HostListener('mouseleave')
   exit() {
-    this.gl.nativeElement.style.borderLeftColor = this.defaultColor;
+    this.hover = false;
   }
 }
