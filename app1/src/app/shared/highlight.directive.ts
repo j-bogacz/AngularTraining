59538ca67126a15f03ec278790c2a7846552f30d
boguslaw <b.pilek@gmail.com>
2017-11-21 12:20:50 +0100

    directive hostListener color getter

--- app1/src/app/shared/highlight.directive.ts
+++ app1/src/app/shared/highlight.directive.ts
@@ -1,30 +1,36 @@
-import { Directive , ElementRef, Input, OnInit, HostListener} from '@angular/core';
+import {Directive, ElementRef, Input, OnInit, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[abcHighlight]'
 })
 export class HighlightDirective {
-
+  hover: boolean;
   @Input('abcHighlight') color: string = 'black';
   @Input() defaultcolor: string = 'green';
+
   constructor(private el: ElementRef) {
     console.log(el);
 
     el.nativeElement.style.borderLeftColor = this.color;
- }
+  }
 
- ngOnInit(){
-   this.el.nativeElement.style.borderLeftColor = this.defaultcolor;
- }
+  ngOnInit() {
+    this.el.nativeElement.style.borderLeftColor = this.defaultcolor;
+  }
 
- @HostListener('mouseenter', ['$event'])
-  mouseenter(){
-   this.el.nativeElement.style.borderLeftColor = this.color;
-   }
+  @HostBinding('style.borderLeftColor')
+  get getColor(){
+    return this.hover ? this.color : this.defaultcolor;
+  }
+
+  @HostListener('mouseenter', ['$event'])
+  mouseenter() {
+    this.hover = true;
+  }
 
   @HostListener('mouseleave', ['$event'])
-    mouseleave(){
-    this.el.nativeElement.style.borderLeftColor = this.defaultcolor;
-    }
+  mouseleave() {
+    this.hover = false;
+  }
 
 }
