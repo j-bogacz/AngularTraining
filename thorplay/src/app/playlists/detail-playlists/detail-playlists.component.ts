9e5a4b419cb31543118128946b495a1aeba7bb27
piotr.buszkiewicz <piotr.buszkiewicz@gmail.com>
2017-11-22 22:06:19 +0100

    poprawiona obsługa list -> detail

--- thorplay/src/app/playlists/detail-playlists/detail-playlists.component.ts
+++ thorplay/src/app/playlists/detail-playlists/detail-playlists.component.ts
@@ -24,8 +24,10 @@ export class DetailPlaylistsComponent implements OnInit {
   @Input() myPlaylist: Playlist;
 
   constructor(private activatedRoute: ActivatedRoute, private  playlistService: PlaylistService) {
-    this.playlistId = parseInt(this.activatedRoute.snapshot.params['id'], 10);
-    this.myPlaylist = this.playlistService.getPlaylistsById(this.playlistId);
+    activatedRoute.params.subscribe(params => {
+      this.playlistId = parseInt(params['id'], 10);
+      this.myPlaylist = this.playlistService.getPlaylistsById(this.playlistId);
+    });
   }
 
   ngOnInit() {
